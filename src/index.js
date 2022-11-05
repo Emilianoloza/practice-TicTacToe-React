109d03fd46a5f613fe5dab7286ee2d33e598e3cd
Emilianoloza <emilianolozac@gmail.com>
2022-11-05 14:39:56 -0600

    add function show winner

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ import './index.css';
 
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -38,7 +41,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
